1196afa3abd8fb4d595e145656c6d4c132f10304
BooleanMage <BooleanMage@users.noreply.github.com>
2024-02-03 12:39:58 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
-git config --local user.name "LatticeMage"
-git config --local user.email "LatticeMage@gmail.com"
+git config --local user.name "BooleanMage"
+git config --local user.email "BooleanMage@users.noreply.github.com"
 git remote set-url origin git@BM:BooleanMage/jekyll-shared-components.git
 
 git pull
